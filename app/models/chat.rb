0c533d50637fdf188873315630710fe7180b6956
Yager47 <ghostship0704@gmail.com>
2025-10-15 14:29:42 +0300

    Change User-Chat relation to HABTM

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,6 @@
 class Chat < ApplicationRecord
-  has_many :users, dependent: :nullify
+  has_many :chat_users, dependent: :destroy
+  has_many :users, through: :chat_users
   has_many :entries, dependent: :destroy
 
   validates :telegram_id, :telegram_type, :title, presence: true
