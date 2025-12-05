b3cb8fb549e354c1ac657ec4648327201a6da35b
Yager47 <ghostship0704@gmail.com>
2025-12-05 19:17:47 +0200

    Add messages. Add history context to AI

--- app/services/ai.rb
+++ app/services/ai.rb
@@ -1,21 +1,30 @@
 class Ai
   class << self
-    def reply_to(message)
+    def reply_in(chat)
       client = OpenAI::Client.new(api_key: ENV["OPENAI_API_KEY"])
 
       response = client.chat.completions.create(
         model: "gpt-5-mini",
-        messages: [
-          { role: "system", content: prompt },
-          { role: "system", content: examples },
-          { role: "user",   content: message }
-        ]
+        messages: messages(chat)
       )
 
       response[:choices][0][:message][:content]
     end
 
     private
+
+    def messages(chat)
+      result = [
+        { role: "system", content: prompt },
+        { role: "system", content: examples }
+      ]
+
+      chat.messages.last(20).each do |message|
+        result << { role: message.role, content: content(message) }
+      end
+
+      result
+    end
 
     def prompt
       File.read Rails.root.join("lib/ai_prompt.txt")
@@ -24,6 +33,11 @@ class Ai
     def examples
       "Деякі приклади відповідей (ігноруй де є rhythm): " \
         "#{File.read(Rails.root.join('lib/answers/chance.yml.erb'))}"
+    end
+
+    def content(message)
+      user_name = message.role == "user" ? "#{message.user.name}: " : ""
+      "#{user_name}\"#{message.content}\""
     end
   end
 end
